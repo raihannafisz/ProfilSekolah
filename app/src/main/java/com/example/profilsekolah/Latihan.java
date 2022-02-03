5c587b2339b65d74b6a64cb22d7b1c46765dc23b
Rizkyamanah <januaranafi@gmail.com>
2022-02-03 11:32:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/profilsekolah/Latihan.java
@@ -0,0 +1,14 @@
+package com.example.profilsekolah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Latihan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_latihan);
+    }
+}
